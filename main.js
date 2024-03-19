fa3e72510398d2cf08acd5e42b636c4db20b1e91
hcferrazALENDA <126292843+hcferrazALENDA@users.noreply.github.com>
2024-03-19 14:57:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+}
+
+function modelLoaded(){
+  console.log('model Loaded');
+}
+function draw(){
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video,gotResul);
+}
+var previous_result ='';
+
+function gotResul(error,results){
+  if (error){
+  } else {
+    if((results[0].confidence >0.5) && (previousResult != results[0].label)){
+      console.log(results);
+      perviousresults = results[0].label;
+      var synth = window.speechSynthesis;
+      speakData = '0 objeto detectado é -'+results.label;
+      var utterThis = new SpeechSynthesisUtterance(speakData);
+    synth.speak(utterThis);
+    document.getElementById("resultObjectName").innerHTML = results[0].label;
+    document.getElementById("resultObjectName Accuracy").innerHTML = results[0].confidense.toFixed(3);
+    }
+  }
+}
+
